dd815fad66f566db599fd2129e8f0b38f1e096d0
jniyonkuru <niyonkurujacques@gmail.com>
2023-07-18 08:59:34 +0200

    add  a button that will be updating the grid size

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 let gridSize=16;
 const container=document.querySelector('.container');
 const div=document.createElement('div');
-div.style.width=`${600/gridSize}px`;
-div.style.height=`${600/gridSize}px`;
+div.style.width=`${400/gridSize}px`;
+div.style.height=`${400/gridSize}px`;
 div.style.border='1px solid black';
 div.classList.add('item');
 for(let i=0;i<gridSize*gridSize;i++){
