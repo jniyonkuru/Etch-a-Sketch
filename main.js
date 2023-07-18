f91ef7aa9e42d569859392bb374c8961b2610057
jniyonkuru <niyonkurujacques@gmail.com>
2023-07-18 13:20:57 +0200

    work on the style , add slidebar that changes the grid dynamically

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 
 let gridSize=16;
+const  slideBar=document.querySelector('input[type="range"]');
+const sizeDisplay=document.querySelector('.size');
 const message=document.querySelector('.message');
 const container=document.querySelector('.container');
 const div=document.createElement('div');
 const button=document.querySelector('#btn');
 div.style.width=`${400/gridSize}px`;
 div.style.height=`${400/gridSize}px`;
-
+sizeDisplay.textContent=`${slideBar.value}X${slideBar.value}`;
 div.classList.add('item');
-button.addEventListener('click',updateCreate)
+slideBar.addEventListener("input",updateGrid)
+
 for(let i=0;i<gridSize*gridSize;i++){
     container.appendChild(div.cloneNode(true));
     
@@ -17,7 +20,7 @@ for(let i=0;i<gridSize*gridSize;i++){
 
 // create a  mouseover event handler function
 function changeGridBackground(e){
-    e.target.style.background='rgb(80, 77, 77)';
+    e.target.style.background='var(--primary-color)';
 }
 // add an eventListener to each grid item;
 
@@ -25,30 +28,27 @@ grids.forEach(div=>div.addEventListener('mouseover',changeGridBackground));
 
 // a  function that update the grid size
 
-function updateCreate(newSize){
-
-    newSize=parseInt(prompt("Enter the the size of the grid  it should be between 0-100"));
-    if(newSize<0||newSize>100){
-     message.innerHTML="The size should be 0-100";
-     return;
-    }
+function updateGrid(e){
+    let newSize=e.target.value;
+    sizeDisplay.textContent=`${newSize}X${newSize}`;
     container.innerHTML="";
-div.style.width=`${400/newSize}px`;
-div.style.height=`${400/newSize}px`;
-div.classList.add('item');
-for(let i=0;i<newSize*newSize;i++){
+    div.style.width=`${400/newSize}px`;
+    div.style.height=`${400/newSize}px`;
+    div.classList.add('item');
+    for(let i=0;i<newSize*newSize;i++){
     container.appendChild(div.cloneNode(true));
     
 }
  const  grids=document.querySelectorAll('.item');
 
 function changeGridBackground(e){
-    e.target.style.background='rgb(80, 77, 77)';
+    e.target.style.background='var(--primary-color)';
 }
 
 grids.forEach(div=>div.addEventListener('mouseover',changeGridBackground));
 
 }
+
 
 
 
