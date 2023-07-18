4a78ccef9f90a1e2abb569a3de156d0bb02420af
jniyonkuru <niyonkurujacques@gmail.com>
2023-07-18 11:01:49 +0200

    update the sytle

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 
-let gridSize=40;
+let gridSize=16;
+const message=document.querySelector('.message');
 const container=document.querySelector('.container');
 const div=document.createElement('div');
+const button=document.querySelector('#btn');
 div.style.width=`${400/gridSize}px`;
 div.style.height=`${400/gridSize}px`;
-div.style.border='1px solid black';
+
 div.classList.add('item');
+button.addEventListener('click',updateCreate)
 for(let i=0;i<gridSize*gridSize;i++){
     container.appendChild(div.cloneNode(true));
     
@@ -14,13 +17,38 @@ for(let i=0;i<gridSize*gridSize;i++){
 
 // create a  mouseover event handler function
 function changeGridBackground(e){
-    e.target.style.background='#000';
+    e.target.style.background='rgb(80, 77, 77)';
 }
 // add an eventListener to each grid item;
 
 grids.forEach(div=>div.addEventListener('mouseover',changeGridBackground));
 
+// a  function that update the grid size
 
+function updateCreate(newSize){
+
+    newSize=parseInt(prompt("Enter the the size of the grid  it should be between 0-100"));
+    if(newSize<0||newSize>100){
+     message.innerHTML="The size should be 0-100";
+     return;
+    }
+    container.innerHTML="";
+div.style.width=`${400/newSize}px`;
+div.style.height=`${400/newSize}px`;
+div.classList.add('item');
+for(let i=0;i<newSize*newSize;i++){
+    container.appendChild(div.cloneNode(true));
+    
+}
+ const  grids=document.querySelectorAll('.item');
+
+function changeGridBackground(e){
+    e.target.style.background='rgb(80, 77, 77)';
+}
+
+grids.forEach(div=>div.addEventListener('mouseover',changeGridBackground));
+
+}
 
 
 
